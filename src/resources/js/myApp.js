5814943c0fc42c2bb26b665c070ec790cb59b19f
pedro germano <pogermano@gmail.com>
2016-04-26 17:19:56 -0300

    Exemplo do part 7

--- src/resources/js/myApp.js
+++ src/resources/js/myApp.js
@@ -1,7 +1,80 @@
 /* use strict */
 var app = angular.module('MyApp', []);
 
-app.directive('interactiveBtn', function(){
+
+app.controller('ShieldCtrl',  function($scope){
+	$scope.shieldNames = [];
+
+
+
+
+this.addReigns = function ()
+{
+
+	$scope.shieldNames.push("Reinado de Jesus!!!");
+
+};
+
+this.addRollings = function ()
+{
+
+	$scope.shieldNames.push("Jesus Sempre vence!!!");
+
+
+};
+
+this.addAmbrose = function ()
+{
+
+	$scope.shieldNames.push("Maranata Jesus!!!");
+};
+
+
+})
+.directive('theshield', function()
+{
+return {
+	restrict: 'E',
+	 scope: {},
+	controller:'ShieldCtrl',
+	link:function(scope,element,attrs)
+	{
+          element.bind('mouseenter',function(){
+	
+			console.log(scope.shieldNames);
+			});
+	}
+}
+
+})
+.directive('reigns', function(){
+	// Runs during compile
+	return {
+		 require: 'theshield', 
+		 link: function(scope,element,attrs, ShieldCtrl) {
+			ShieldCtrl.addReigns();
+		}
+	};
+})
+.directive('rollings', function(){
+	// Runs during compile
+	return {
+		 require: 'theshield', 
+		 link: function(scope,element,attrs, ShieldCtrl) {
+			ShieldCtrl.addRollings();
+		}
+	};
+})
+.directive('ambrose', function(){
+	// Runs during compile
+	return {
+		 require: 'theshield', 
+		 link: function(scope,element,attrs, ShieldCtrl) {
+			ShieldCtrl.addAmbrose();
+		}
+	};
+})
+/*.directive('interactiveBtn', function(){
 	// Runs during compile
 	return {
 		// name: '',
@@ -18,18 +91,19 @@ app.directive('interactiveBtn', function(){
 		// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
 		link: function($scope, iElm, iAttrs, controller) {
 			iElm.bind('mouseenter',function(){
-				console.log("Teste1");
+			/*	console.log("Teste1");
 				console.log(iElm);
-				iElm[0].innerText="Entrou";
+				iElm[0].innerText="Entrou";* /
 			})
-				iElm.bind('mouseleave',function(){
+			/*	iElm.bind('mouseleave',function(){
 				console.log("Teste1");
 				console.log(iElm);
 				iElm[0].innerText="Saiu";
-			});
+			});* /
 		}
 	};
-});
+});*/
+
 
 /*app.directive('walterwhite', function()
 {
