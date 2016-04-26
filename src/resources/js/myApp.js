0b48e6ebd37039b0c90edfb82fcdb2ebcdde73a0
pedro germano <pogermano@gmail.com>
2016-04-26 14:27:08 -0300

    Exemplo do part 5

--- src/resources/js/myApp.js
+++ src/resources/js/myApp.js
@@ -3,10 +3,18 @@ var app = angular.module('MyApp', []);
 
 app.directive('walterwhite', function()
 {
-	// Runs during compile
 	return {
 		 restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
 		 transclude: true,
+		 link: function(scope,element,attribute){
+		 	console.log(scope);
+		 	console.log(element);
+		 	console.log(attribute);
+		 }
+		 		} 
+	/*return {
+		 restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
+		 transclude: true,
 		 template: '<h2> TUCA </h2>'
-		}
+		} */
 	});
