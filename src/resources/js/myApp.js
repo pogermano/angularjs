a0de977362d44d57dd66a6839a03a9c1520ed1dd
pedro germano <pogermano@gmail.com>
2016-04-26 15:32:26 -0300

    Exemplo do part 6

--- src/resources/js/myApp.js
+++ src/resources/js/myApp.js
@@ -1,7 +1,37 @@
 /* use strict */
 var app = angular.module('MyApp', []);
 
-app.directive('walterwhite', function()
+app.directive('interactiveBtn', function(){
+	// Runs during compile
+	return {
+		// name: '',
+		// priority: 1,
+		// terminal: true,
+		// scope: {}, // {} = isolate, true = child, false/undefined = no change
+		// controller: function($scope, $element, $attrs, $transclude) {},
+		// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+		 restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
+		// template: '',
+		// templateUrl: '',
+		// replace: true,
+		// transclude: true,
+		// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+		link: function($scope, iElm, iAttrs, controller) {
+			iElm.bind('mouseenter',function(){
+				console.log("Teste1");
+				console.log(iElm);
+				iElm[0].innerText="Entrou";
+			})
+				iElm.bind('mouseleave',function(){
+				console.log("Teste1");
+				console.log(iElm);
+				iElm[0].innerText="Saiu";
+			});
+		}
+	};
+});
+
+/*app.directive('walterwhite', function()
 {
 	return {
 		 restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
@@ -16,5 +46,5 @@ app.directive('walterwhite', function()
 		 restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
 		 transclude: true,
 		 template: '<h2> TUCA </h2>'
-		} */
-	});
+		} * /
+	});*/
